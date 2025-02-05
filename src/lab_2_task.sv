2a011cd9e7bfe69b25cbfdaeec81e955b9d7463a
RaadMahmood <raadmahmood@github.com>
2025-02-05 22:49:28 +0500

    Lab 2 Uploaded

---
+++ src/lab_2_task.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/31/2025 02:54:29 PM
+// Design Name: 
+// Module Name: lab_2_task
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module lab_2_task(output logic x,y,
+input logic a,b,c
+  );
+
+  assign x = (~c) ^ (a|b);
+  assign y = ((a|b) ^ ~((a&b)&(a|b)));
+  
+endmodule
